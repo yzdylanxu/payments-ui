ed9992813e96fbbe45eb7b01a933e6fd7ddf1874
vppmatt <matt@multicode.co.uk>
2023-06-06 14:54:11 +0100

    removing warnings

--- src/data/DataFunctions.js
+++ src/data/DataFunctions.js
@@ -6,21 +6,19 @@ if (process.env.COMPONENT_QUARKUS_BACKEND_HOST) {
 }
 
 export const getAllPaymentsForCountry = (country) => {
-    const transactionsPromise = axios({url : `https://${serverURL}/api/cctransaction?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
-    return transactionsPromise;
+    return axios({url : `https://${serverURL}/api/cctransaction?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const getAllPaymentsForOrderId = (orderId) => {
-    const transactionsPromise = axios({url : `https://${serverURL}/api/cctransaction?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
-    return transactionsPromise;
+    return axios({url : `https://${serverURL}/api/cctransaction?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const getCountries = () => {
-    return axios.request({url : "http://${serverURL}/api/countries", method: "GET", headers : {'Accept': 'application/json'} });
+    return axios.request({url : `http://${serverURL}/api/countries`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const addNewTransaction = (payment) => {
-    return axios({url : "http://${serverURL}/api/cctransaction",
+    return axios({url : `http://${serverURL}/api/cctransaction`,
         method: "POST",
         headers : {'Accept': 'application/json', 'Content-Type' : 'application/json'},
         data: payment});
