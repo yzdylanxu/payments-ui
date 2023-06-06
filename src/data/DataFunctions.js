aa74252260a5e567f9ff60d008f666f4f65cd858
vppmatt <matt@multicode.co.uk>
2023-06-06 13:34:45 +0100

    amending url for os

--- src/data/DataFunctions.js
+++ src/data/DataFunctions.js
@@ -1,22 +1,28 @@
 import axios from 'axios';
 
+let serverURL = localhost;
+if (process.env.COMPONENT_QUARKUS_BACKEND_HOST) {
+   serverURL=process.env.COMPONENT_QUARKUS_BACKEND_HOST
+}
+
 export const getAllPaymentsForCountry = (country) => {
-    const transactionsPromise = axios({url : `https://paymentgateway-standalone-payment-gateway.apps.openshift4demo.xqzk.p1.openshiftapps.com/api/cctransaction?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
+    const transactionsPromise = axios({url : `https://${serverURL}/api/cctransaction?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
     return transactionsPromise;
 }
 
 export const getAllPaymentsForOrderId = (orderId) => {
-    const transactionsPromise = axios({url : `https://paymentgateway-standalone-payment-gateway.apps.openshift4demo.xqzk.p1.openshiftapps.com/api/cctransaction?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
+    const transactionsPromise = axios({url : `https://${serverURL}/api/cctransaction?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
     return transactionsPromise;
 }
 
 export const getCountries = () => {
-    return axios.request({url : "http://paymentgateway-standalone-payment-gateway.apps.openshift4demo.xqzk.p1.openshiftapps.com/api/countries", method: "GET", headers : {'Accept': 'application/json'} });
+    return axios.request({url : "http://${serverURL}/api/countries", method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const addNewTransaction = (payment) => {
-    return axios({url : "http://paymentgateway-standalone-payment-gateway.apps.openshift4demo.xqzk.p1.openshiftapps.com/api/cctransaction",
+    return axios({url : "http://${serverURL}/api/cctransaction",
         method: "POST",
         headers : {'Accept': 'application/json', 'Content-Type' : 'application/json'},
         data: payment});
 }
+
