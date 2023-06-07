36d44fb7c7a3150e7c739e42ffc283604551ed83
vppmatt <matt@multicode.co.uk>
2023-06-07 14:11:31 +0100

    fixing urls

--- src/data/DataFunctions.js
+++ src/data/DataFunctions.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 let serverURL = "https://payment-gateway-mattt-payments.apps.openshift4demo.xwun.p1.openshiftapps.com";
 
 export const getAllPaymentsForCountry = (country) => {
-    return axios({url : `${serverURL}/api/cctransaction?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
+    return axios({url : `${serverURL}/api/payment?country=${country}`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const getAllPaymentsForOrderId = (orderId) => {
-    return axios({url : `${serverURL}/api/cctransaction?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
+    return axios({url : `${serverURL}/api/payment?order=${orderId}`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const getCountries = () => {
-    return axios.request({url : `${serverURL}/api/countries`, method: "GET", headers : {'Accept': 'application/json'} });
+    return axios.request({url : `${serverURL}/api/country`, method: "GET", headers : {'Accept': 'application/json'} });
 }
 
 export const addNewTransaction = (payment) => {
-    return axios({url : `${serverURL}/api/cctransaction`,
+    return axios({url : `${serverURL}/api/payment`,
         method: "POST",
         headers : {'Accept': 'application/json', 'Content-Type' : 'application/json'},
         data: payment});
